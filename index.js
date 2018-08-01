b0eb46751d7f671514d6027cdb6b32d730092f41
Marco Solazzi <marco.solazzi@gmail.com>
2018-08-02 00:21:04 +0900

    component

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+const Item = {
+    props: {
+        text: String
+    },
+    template: `<li class="list-group-item">{{text}}</li>`
+}
+
+const AddForm = {
+    template: `
+    <form @submit.prevent="add">
+        <div class="form-group">
+            <input class="form-control" type="text" v-model="text">
+        </div>
+        <button class="btn btn-primary" type="submit" :disabled="text.length < 1">Add</button>
+    </form>
+    `,
+    data: () => ({ text: '' }),
+    props: {
+        onSubmit: Function
+    },
+    methods: {
+        add() {
+            this.onSubmit(this.text)
+            this.text = ''
+        }
+    }
+}
 new Vue({
     el: "#app",
     data: {
-      text: "",
       items: []
     },
+    components: {
+        Item,
+        AddForm
+    },
     methods: {
-      add() {
-        this.items.push(this.text);
-        this.text = "";
+      addItem(value) {
+        this.items.push(value);
       }
     }
   });
